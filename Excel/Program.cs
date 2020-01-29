f3e28c84802fbf39d52a8cd445e7db1df0ab893e
BrankoMalbasic <brankomalbasic@gmail.com>
2020-01-29 09:04:51 +0100

    Add files via upload

---
+++ Excel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace Excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("excel");
+        }
+    }
+}
